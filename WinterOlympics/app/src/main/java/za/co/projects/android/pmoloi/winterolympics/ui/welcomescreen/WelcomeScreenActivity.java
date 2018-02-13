0fc19d538c58df8d5c3fe662cbb7878fe7c3c35c
Paballo Moloi <PMoloi@jhb.dvt.co.za>
2018-02-13 13:29:02 +0200

    Welcome screen layout

---
+++ WinterOlympics/app/src/main/java/za/co/projects/android/pmoloi/winterolympics/ui/welcomescreen/WelcomeScreenActivity.java
@@ -0,0 +1,22 @@
+package za.co.projects.android.pmoloi.winterolympics.ui.welcomescreen;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import za.co.projects.android.pmoloi.winterolympics.R;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+    }
+}
+
