7f35ab02eb9e5a8ad7c8293d60849cc297428706
Pabi <pmoloi@jhb.dvt.co.za>
2018-04-18 06:14:19 +0200

    Addition of files for the schedule feature

--- app/src/main/java/za/co/projects/android/pmoloi/winterolympics/ui/welcomescreen/WelcomeScreenActivity.java
+++ app/src/main/java/za/co/projects/android/pmoloi/winterolympics/ui/welcomescreen/WelcomeScreenActivity.java
@@ -1,10 +1,12 @@
 package za.co.projects.android.pmoloi.winterolympics.ui.welcomescreen;
 
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import za.co.projects.android.pmoloi.winterolympics.R;
 
@@ -17,6 +19,11 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+    }
+
+    public void onExploreClick(View view)
+    {
+        startActivity(new Intent(this,HomeActivity.class));
     }
 }
 
