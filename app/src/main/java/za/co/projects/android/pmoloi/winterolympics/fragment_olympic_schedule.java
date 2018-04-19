6e253efcde29bc4ec737fbc5623cdbbec3926e89
Pabi <pmoloi@jhb.dvt.co.za>
2018-04-19 10:01:00 +0200

    Refactoring - File rename

--- app/src/main/java/za/co/projects/android/pmoloi/winterolympics/fragment_olympic_schedule.java
+++ app/src/main/java/za/co/projects/android/pmoloi/winterolympics/fragment_olympic_schedule.java
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
 
 
 /**
@@ -65,7 +64,7 @@ public class fragment_olympic_schedule extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_olympic_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_olympic_schedule, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
